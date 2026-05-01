cd6a60c824481448f0dae10a6b51e7304911bc82
Valerrriy Orlov <wrestlbird@gmail.com>
2026-05-01 13:15:45 +0300

    fix: добавлены теги json для CreatedAt и UpdatedAt

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type User struct {
-	ID           uint   `gorm:"primaryKey" json:"id"`
-	Email        string `gorm:"uniqueIndex;not null" json:"email"`
-	Username     string `gorm:"uniqueIndex;not null" json:"username"`
-	PasswordHash string `gorm:"not null" json:"-"`
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	Email        string    `gorm:"uniqueIndex;not null" json:"email"`
+	Username     string    `gorm:"uniqueIndex;not null" json:"username"`
+	PasswordHash string    `gorm:"not null" json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
